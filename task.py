fb798069cc58d20b52152778964dd50be72cdd3c
Gavin Wilson <68349014+EyeAmTheOne@users.noreply.github.com>
2025-04-27 21:51:17 -0700

    Successfully capture one command-line arg using argparse

--- task.py
+++ task.py
@@ -1,5 +1,12 @@
+import argparse
+
 def main():
-    print("Hello, World!")
+    # Capture a command-line argument and print it out to the console
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", type=str, help="Name of the person to greet")
+    args = parser.parse_args()
+
+    print(f"Hello, {args.name}!")
 
 if __name__ == "__main__":
     main()
